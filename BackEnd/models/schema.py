6d2b397fd64c8161eb154316d862f8456da14543
Luis Miguel Torres Abonce <luis.torres@profuturo.com.mx>
2026-04-16 17:16:13 -0600

    Manejo de variables de entorno

--- BackEnd/models/schema.py
+++ BackEnd/models/schema.py
@@ -1,9 +1,13 @@
+import os
+
 from pydantic import BaseModel
 from typing import List
-
+import dotenv
+dotenv.load_dotenv()
+TOP_K = int(os.getenv("TOP_K"))
 class SearchRequest(BaseModel):
     query: str
-    top: int = 3
+    top: int = TOP_K
 
 class Chunk(BaseModel):
     id: str
--- BackEnd/rag/pipeline.py
+++ BackEnd/rag/pipeline.py
@@ -1,10 +1,14 @@
 from rag.retriever import search_chunks
 from rag.prompt_builder import build_prompt
 from rag.generator import generate_answer_stream
+import os
+import dotenv
+dotenv.load_dotenv()
+TOP_K = int(os.getenv("TOP_K"))
 
 def run_rag_stream(query: str, rag: str):
     """Retorna los chunks y un generador de eventos (tokens + tokens_info) para streaming"""
-    result = search_chunks(type("obj", (object,), {"query": query, "top": 3, "rag": rag}))
+    result = search_chunks(type("obj", (object,), {"query": query, "top": TOP_K, "rag": rag}))
     chunks = result["chunks"]
     prompt = build_prompt(query, chunks, rag)
     token_generator = generate_answer_stream(prompt)
